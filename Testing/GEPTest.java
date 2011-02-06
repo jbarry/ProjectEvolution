86ac3175373d6d28826a9d221522e35f5a7ee0bb
JustinBarry <justin.barry.06@cnu.edu>
2011-02-06 14:40:40 -0500

    Added GEPTest.java under the Testing package.

---
+++ Testing/GEPTest.java
@@ -0,0 +1,17 @@
+package Testing;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class GEPTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
