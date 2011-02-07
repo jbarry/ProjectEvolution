9d4669be1f341c5841f328e1e8bd844dd8a8dc9f
cjackey <christopher.jackey.07@cnu.edu>
2011-02-07 13:39:01 -0800

    Updated paint method.

--- Interactive/Organism.java
+++ Interactive/Organism.java
@@ -10,11 +10,11 @@ public class Organism {
 	//------------------------------------------------------------------------------------
 	//--globals--
 	//------------------------------------------------------------------------------------
-	private double     health;
+	private double health;
 	private Coordinate location;
 	private Chromosome chromosome;
-	private double     fitness;
-	
+	private double fitness;
+
 	//------------------------------------------------------------------------------------
 	//--constructors--
 	//------------------------------------------------------------------------------------
@@ -22,37 +22,37 @@ public class Organism {
 		health = 100.00;
 		//location (x, y) is random between (0-width,0-height) inclusive
 		Random r = new Random();
-		location   = new Coordinate(r.nextInt(GridPanel.WIDTH + 1), 
-									r.nextInt(GridPanel.HEIGHT + 1));
+		location = new Coordinate(r.nextInt(GridPanel.WIDTH + 1),
+				r.nextInt(GridPanel.HEIGHT + 1));
 		chromosome = new Chromosome();
 		fitness=0.0;
 	}
-	
+
 	public Organism(Coordinate aLocation, Chromosome aChromosome){
-		health     = 100.0;
-		location   = aLocation;
+		health = 100.0;
+		location = aLocation;
 		chromosome = aChromosome;
 	}
-	
+
 	//------------------------------------------------------------------------------------
 	//--getters/setters--
 	//------------------------------------------------------------------------------------
 	public Coordinate getLocation(){
 		return location;
 	}
-	
+
 	public Chromosome getChromosome(){
 		return chromosome;
 	}
-	
+
 	public double getFitness(){
 		return fitness;
 	}
-	
+
 	public void setFitness(double aFit){
 		fitness=aFit;
 	}
-	
+
 	public double getHealth(){
 		return health;
 	}
@@ -62,34 +62,34 @@ public class Organism {
 	public void moveNorth(){
 		location.setY(location.getY() - 1);
 	}
-	
+
 	public void moveNorthEast(){
 		location.setX(location.getX() + 1);
 		location.setY(location.getY() - 1);
 	}
-	
+
 	public void moveEast(){
 		location.setX(location.getX() + 1);
 	}
-	
+
 	public void moveSouthEast(){
 		location.setX(location.getX() + 1);
 		location.setY(location.getY() + 1);
 	}
-	
+
 	public void moveSouth(){
 		location.setY(location.getY() + 1);
 	}
-	
+
 	public void moveSouthWest(){
 		location.setX(location.getX() - 1);
 		location.setY(location.getY() + 1);
 	}
-	
+
 	public void moveWest(){
 		location.setX(location.getX() - 1);
 	}
-	
+
 	public void moveNorthWest(){
 		location.setX(location.getX() - 1);
 		location.setY(location.getY() - 1);
@@ -98,9 +98,9 @@ public class Organism {
 	public void paint(Graphics g)
 	{
 		g.setColor(Color.BLACK);
-		g.fillRect((int)this.getLocation().getX(), (int)this.getLocation().getY(), 5, 5);
+		g.fillRect((int)this.getLocation().getX()-3, (int)this.getLocation().getY()-3, 6, 6);
 	}
-	
+
 	//------------------------------------------------------------------------------------
 	//--overloaded functions--
 	//------------------------------------------------------------------------------------
