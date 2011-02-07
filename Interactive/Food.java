31c0a550f72b9a38fe7d4987c1ce17ce53a844a9
cjackey <christopher.jackey.07@cnu.edu>
2011-02-07 13:38:03 -0800

    Updated paint method.

--- Interactive/Food.java
+++ Interactive/Food.java
@@ -13,54 +13,60 @@ public class Food {
 	private Coordinate location;
 	private double foodRemaining;
 	private Random r;
-	
+
 	//------------------------------------------------------------------------------------
 	//--constructors--
 	//------------------------------------------------------------------------------------
 	public Food(){
 		this.r = new Random();
-		this.location=new Coordinate(r.nextInt(GridPanel.WIDTH + 1), 
-		r.nextInt(GridPanel.HEIGHT + 1));
-		this.foodRemaining=100.0;	
+		this.location=new Coordinate(r.nextInt(GridPanel.WIDTH + 1),
+				r.nextInt(GridPanel.HEIGHT + 1));
+		this.foodRemaining=100.0;
 	}
-	
+
 	public Food(int x, int y){
 		this.r = new Random();
 		this.location=new Coordinate(x,y);
-		this.foodRemaining=100.0;	
+		this.foodRemaining=100.0;
 	}
-	
+
 	public Food(Coordinate aCoord){
 		this.r = new Random();
 		this.location=aCoord;
-		this.foodRemaining=100.0;	
+		this.foodRemaining=100.0;
 	}
-	
+
 	//------------------------------------------------------------------------------------
 	//--getters/setters--
 	//------------------------------------------------------------------------------------
 	public double getFoodRemaining(){
 		return foodRemaining;
 	}
-	
+
 	public Coordinate getLocation(){
 		return location;
 	}
-	
+
 	//------------------------------------------------------------------------------------
 	//--accessors/mutators--
 	//------------------------------------------------------------------------------------
 	public void deplete(){
 		if(foodRemaining>=0){
-		foodRemaining--;
+			foodRemaining--;
 		}
 	}
-	
-	public void paint(Graphics g)
+
+	public void paint(Graphics g, boolean isDepleted)
 	{
 		g.setColor(Color.BLUE);
-		g.fillRect((int)this.getLocation().getX(), (int)this.getLocation().getY(), 5, 5);
+		
+		if(isDepleted){
+			g.drawRect((int)this.getLocation().getX()-3, (int)this.getLocation().getY()-3, 6, 6);
+		}
+		else{
+			g.fillRect((int)this.getLocation().getX()-3, (int)this.getLocation().getY()-3, 6, 6);
+		}
 	}
-	
-	
+
+
 }
