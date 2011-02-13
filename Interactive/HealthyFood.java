2af06c4abf78ffc793f1f61c52f79a80cefb22a4
LetThereBeDwight <dwight.bell1@gmail.com>
2011-02-13 17:23:49 -0500

    adding subclasses of Food, healthy/poisonous

---
+++ Interactive/HealthyFood.java
@@ -0,0 +1,39 @@
+package Interactive;
+import java.awt.Color;
+import java.awt.Graphics;
+import Frame.Coordinate;
+
+public class HealthyFood extends Food {
+	//------------------------------------------------------------------------------------
+	//--constructors--
+	//------------------------------------------------------------------------------------
+	public HealthyFood(){}
+
+	public HealthyFood(int x, int y){
+		super(x,y);
+	}
+
+	public HealthyFood(Coordinate aCoord){
+		super(aCoord);
+	}
+
+	@Override
+	public String toString() {
+		String str = "";
+		str += "I am food. Eat me."
+			+ "\nLocation: " + getLocation();
+		return str;		
+	}
+
+	@Override
+	public void paint(Graphics g, boolean isDepleted) {
+		g.setColor(Color.BLUE);
+
+		if(!isDepleted){
+			g.fillRect((int)this.getLocation().getX()-2, (int)this.getLocation().getY()-2, 5, 5);
+		}
+
+	}
+
+
+}
