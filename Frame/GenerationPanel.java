bb0ac70660977370647a2cfad0cd7baaced93e99
cjackey <christopher.jackey.07@cnu.edu>
2011-03-07 14:21:07 -0800

    Scroll-bar for the past generation's average fitness finally works! FU scrollbar, FU...

--- Frame/GenerationPanel.java
+++ Frame/GenerationPanel.java
@@ -1,7 +1,9 @@
 package Frame;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -9,10 +11,15 @@ import java.awt.event.WindowEvent;
 import java.util.Random;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoundedRangeModel;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import Interactive.Food;
 import Interactive.Organism;
@@ -24,6 +31,7 @@ public class GenerationPanel extends JPanel{
 	private JLabel genTitle;
 	private JTextArea currentGeneration;
 	private JTextArea pastGenerations;
+	private JTextArea pastGenStats;
 	private GridPanel sim;
 	
 	//------------------------------------------------------------------------------------
@@ -50,21 +58,41 @@ public class GenerationPanel extends JPanel{
 		currentGeneration.setLayout(null);
 		currentGeneration.setEditable(false);
 		currentGeneration.setSize(GenerationPanel.WIDTH, 40);
-		currentGeneration.setText(" Current Generation: " + sim.generationNum);
-		currentGeneration.append("\n" + " Current Trial: " + sim.trialNum);
+		currentGeneration.setText("  Current Generation: " + sim.generationNum);
+		currentGeneration.append("\n" + "  Current Trial: " + sim.trialNum);
 		currentGeneration.setLocation(0,21);
-		pastGenerations= new JTextArea(100,100);
 		
+		pastGenerations= new JTextArea(3, 100);
 		pastGenerations.setOpaque(false);
 		pastGenerations.setLayout(null);
 		pastGenerations.setEditable(false);
 		pastGenerations.setSize(GenerationPanel.WIDTH*2, GenerationPanel.HEIGHT-40);
-		pastGenerations.setText("\n" + " Past Generation Number-Average Fitness ");
+		pastGenerations.setText("\n" + "  Past Generation Number-Average Fitness:");
 		pastGenerations.setLocation(0,42);
-		JScrollPane scrollPane = new JScrollPane(pastGenerations);
+		
+		pastGenStats = new JTextArea();
+		
+		JScrollPane scrollPane = new JScrollPane(pastGenStats);
+		JPanel pastGenPanel = new JPanel();
+		pastGenPanel.setOpaque(false);
+		pastGenPanel.setLayout(new GridLayout());
+		pastGenPanel.setFocusable(true);
+		pastGenPanel.setSize(233, 70);
+		pastGenPanel.setLocation(5,75);
+		
+		pastGenPanel.add(scrollPane);
+		
+		/**
+		 * Need to add nested components before parent components.
+		 */
+		add(pastGenPanel);
+		pastGenPanel.add(scrollPane);
+		
 		add(genTitle);
 		add(currentGeneration);
 		add(pastGenerations);
+		
+		
 	}
 	
 	public void newTrial(){
@@ -81,8 +109,11 @@ public class GenerationPanel extends JPanel{
 	}
 	
 	public void addGeneration(){
-		if(sim.generationNum<60)
-		pastGenerations.append("\n" + " Generation " + (sim.generationNum-1) + ": " + sim.lastAvg);
-		
+		if(sim.generationNum<60 && pastGenStats.getText().equals("")){
+			pastGenStats.append(" Generation " + (sim.generationNum-1) + ": " + sim.lastAvg);
+		}
+		else if(sim.generationNum<60){
+			pastGenStats.append("\n" + " Generation " + (sim.generationNum-1) + ": " + sim.lastAvg);
+		}
 	}
 }
