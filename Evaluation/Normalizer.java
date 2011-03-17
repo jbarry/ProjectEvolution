9b013e6f888cbed0ecc555ef4a93d052884d7dee
JustinBarry <justin.barry.06@cnu.edu>
2011-03-17 19:50:00 -0400

    The Evaluation/Normalizer class is now tested and working fully.

--- Evaluation/Normalizer.java
+++ Evaluation/Normalizer.java
@@ -1,29 +1,60 @@
 package Evaluation;
 import Interactive.Pair;
+import static java.lang.System.out;
 
+/**
+*This class can be instantiated or we
+*can use that static method normalize().
+*This class can be used more efficiently if
+*it is instantiated.
+*/
 public class Normalizer {
+	
+	public static void main(String[] args) {
+//		for(int i = 1; i < 167; i++) {
+//			out.println(Normalizer.normalize(
+//					new Pair<Double, Double> (1.0, 167.0), 
+//					new Pair<Double, Double> (1.0, 53.0), i));
+//		}
+//		Normalizer norm = new Normalizer
+//		(new Pair<Double, Double> (1.0, 167.0),
+//				new Pair<Double, Double> (1.0, 53.0));
+//		for(int i = 1; i < 167; i++) {
+//			out.println(norm.normalize(i));
+//		}
+	}
 
 	private Pair<Double, Double> fromRange;
 	private Pair<Double, Double> toRange;
-
+	private double preCalc;
+	
+	public Normalizer() {
+		fromRange = null;
+		toRange = null;
+	}
 	public Normalizer(Pair<Double, Double> aFromRange,
 			Pair<Double, Double> aToRange) {
 		fromRange = aFromRange;
 		toRange = aToRange;
-	}
-
-	public static void main(String[] args) {
+		double a = fromRange.left();
+		double b = fromRange.right();
+		double c = toRange.left();
+		double d = toRange.right();
+		preCalc = (d-c)/(b-a);
 	}
 
 	public static Double normalize(Pair<Double, Double> aFromRange,
-			Pair<Double, Double> aToRange) {
-		
-		return 0.0;
+			Pair<Double, Double> aToRange, double x) {
+		double a = aFromRange.left();
+		double b = aFromRange.right();
+		double c = aToRange.left();
+		double d = aToRange.right();
+		return 1 + (x-a)*(d-c)/(b-a);
 	}
 
-	public Double normalize() {
-
-		return 0.0;
+	public Double normalize(double x) {
+		double a = fromRange.left();
+		return 1 + (x-a)*preCalc;
 	}
 
 	public void setFromRange(Pair<Double, Double> aFromRange) {
