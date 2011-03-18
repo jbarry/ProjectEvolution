152523b118ec548a559d27d0f0e455f5683ff393
jbarry <justin.barry.06@cnu.edu>
2011-03-18 03:52:42 -0700

    Edited Interactive/Matter.java via GitHub

--- Interactive/Matter.java
+++ Interactive/Matter.java
@@ -5,7 +5,21 @@ import Frame.Coordinate;
 public abstract class Matter {
 	protected Coordinate location;
 	protected double health;
-	protected double maxHealth;
+	protected double mxHlth;
+	protected int id;
+	
+	public Matter() {
+		
+	}
+	
+	public Matter(double aMxHlth) {
+		health = mxHlth = aMxHlth;
+	}
+	
+	public Matter(double aMxHlth, int anId) {
+		health = mxHlth = aMxHlth;
+		id = anId;
+	}
 	
 	public abstract double numSurroundingObjects(int scanRange);
 }
