96402c70f437eb82ad9d728eab9e43ef62a1a539
JustinBarry <justin.barry.06@cnu.edu>
2011-04-09 12:16:06 -0400

    Fixed the ctor for LocationMap and add the LocationMapTest class in the Testing package. Trying to find out why orgs and foods are not being placed in the LocationMap.

---
+++ Testing/LocationMapTest.java
@@ -0,0 +1,31 @@
+package Testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import Frame.LocationMap;
+
+public class LocationMapTest {
+
+	LocationMap map = LocationMap.getInstance();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
